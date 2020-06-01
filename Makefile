cc48f841556d5fba42a6a5d5c6bbff35bf6fd83a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-01 12:46:48 +0200

    Create English phylogeny

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 all: summary.pdf
 
-summary.pdf: *.tex
+summary.pdf: *.tex phylogeny.png
 	aspell -t -c summary.tex
 	./create.sh
 	#./view.sh
+
+phylogeny.png: create_phylogeny.R
+	Rscript create_phylogeny.R
+
 
---
+++ create_phylogeny.R
@@ -0,0 +1,14 @@
+# Create the phylogeny used in the English summary
+
+library(ape)
+library(ggtree)
+
+phylogeny <- ape::read.tree(text = "((human:1,gorrila:1):1,dolphin:2):1;")
+
+ggtree::ggtree(phylogeny, cex = 2) +
+  ggtree::geom_tiplab(
+    geom = "label", cex = 10.0, offset = 0.5, hjust = .8
+  ) +
+  ggtree::geom_rootedge(cex = 2) +
+  ggplot2::ggsave("phylogeny.png")
+
