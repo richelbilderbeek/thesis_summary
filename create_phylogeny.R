f4980057880b95e89813d31bb6e64ef8e90cf6f7
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-17 15:21:57 +0200

    Cannot do 'ggplot2::ggplot(...) + ggplot2::ggsave(...)', do 'ggplot2::ggplot(...); ggplot2::ggsave(...)' instead, see https://github.com/tidyverse/ggplot2/issues/4517

--- create_phylogeny.R
+++ create_phylogeny.R
@@ -9,6 +9,5 @@ ggtree::ggtree(phylogeny, cex = 2) +
   ggtree::geom_tiplab(
     geom = "label", cex = 10.0, offset = 0.5, hjust = .8
   ) +
-  ggtree::geom_rootedge(cex = 2) +
-  ggplot2::ggsave("summary_phylogeny.png")
+  ggtree::geom_rootedge(cex = 2); ggplot2::ggsave("summary_phylogeny.png")
 
